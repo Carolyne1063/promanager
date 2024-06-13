d0c8a8bab6e2f96ddeba9f2ab2489d3d42a3378f
Carolyne1063 <carolmartoz763@gmail.com>
2024-06-13 16:44:21 -0700

    added login-form validators

--- promanager/src/app/components/login/login.component.ts
+++ promanager/src/app/components/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, ReactiveFormsModule, NavbarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  email: string ='';
-  password : string ='';
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email() { return this.loginForm.get('email')!; }
+  get password() { return this.loginForm.get('password')!; }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted!', this.loginForm.value);
+    }
+  }
 }
